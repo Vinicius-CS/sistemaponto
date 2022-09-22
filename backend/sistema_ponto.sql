4c75f407edb62411010ce8a75d99da76c4b30b7a
Vinicius-CS <vinicius--cruz@hotmail.com>
2022-09-21 22:45:24 -0300

    Alteração na SQL

--- backend/sistema_ponto.sql
+++ backend/sistema_ponto.sql
@@ -6,14 +6,14 @@ CREATE TABLE IF NOT EXISTS `user` (
  	`name` varchar(255) NOT NULL,
   	`email` varchar(255) NOT NULL UNIQUE,
   	`password` varchar(255) NOT NULL,
-    `admin` ENUM('false', 'true') DEFAULT 'false',
-	`enabled` ENUM('false', 'true') DEFAULT 'true'
+    `admin` ENUM('false', 'true') DEFAULT 'false' NOT NULL,
+	`enabled` ENUM('false', 'true') DEFAULT 'true' NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `time_sheet` (
 	`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
  	`start` DATETIME NOT NULL,
-  	`end` DATETIME NOT NULL UNIQUE,
+  	`end` DATETIME UNIQUE DEFAULT NULL,
     `user_id` int(11) NOT NULL,
     FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
 );
@@ -22,3 +22,13 @@ INSERT INTO `user` (`name`, `email`, `password`, `admin`, `enabled`) VALUES ('Ad
 INSERT INTO `user` (`name`, `email`, `password`, `admin`, `enabled`) VALUES ('Colaborador 1', 'colaborador1@sistemaponto.com', MD5('clbr1'), 'false', 'true');
 INSERT INTO `user` (`name`, `email`, `password`, `admin`, `enabled`) VALUES ('Colaborador 2', 'colaborador2@sistemaponto.com', MD5('clbr2'), 'false', 'true');
 INSERT INTO `user` (`name`, `email`, `password`, `admin`, `enabled`) VALUES ('Colaborador 3', 'colaborador3@sistemaponto.com', MD5('clbr3'), 'false', 'true');
+
+INSERT INTO `time_sheet` (`start`, `end`, `user_id`) VALUES ('2022-09-20 08:00:00', '2022-09-20 18:30:00', '1');
+INSERT INTO `time_sheet` (`start`, `end`, `user_id`) VALUES ('2022-09-20 08:30:00', '2022-09-20 18:00:00', '2');
+INSERT INTO `time_sheet` (`start`, `end`, `user_id`) VALUES ('2022-09-20 08:35:00', '2022-09-20 18:05:00', '3');
+INSERT INTO `time_sheet` (`start`, `end`, `user_id`) VALUES ('2022-09-20 08:40:00', '2022-09-20 18:10:00', '4');
+
+INSERT INTO `time_sheet` (`start`, `end`, `user_id`) VALUES ('2022-09-21 08:00:00', '2022-09-21 18:30:00', '1');
+INSERT INTO `time_sheet` (`start`, `end`, `user_id`) VALUES ('2022-09-21 08:40:00', '2022-09-21 18:05:00', '2');
+INSERT INTO `time_sheet` (`start`, `end`, `user_id`) VALUES ('2022-09-21 08:30:00', '2022-09-21 18:10:00', '3');
+INSERT INTO `time_sheet` (`start`, `end`, `user_id`) VALUES ('2022-09-21 08:35:00', '2022-09-21 18:15:00', '4');
